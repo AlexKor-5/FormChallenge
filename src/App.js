e142f5db5796742033bd0a164f591c2d02116abf
alexander <world.kor.films@gmail.com>
2022-01-17 15:01:24 +0300

    the little working phone number input (src/components/PhoneNumberInput)

--- src/App.js
+++ src/App.js
@@ -9,15 +9,15 @@ import { TextInput } from './components/TextInput/TextInput'
 import { MyButton } from './components/MyButton/MyButton'
 import { PhoneNumberInput } from './components/PhoneNumberInput/PhoneNumberInput'
 
-const validate = values => {
-    const errors = {}
-    if (!values.email) {
-        errors.email = 'Required'
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-        errors.email = 'Invalid email address'
-    }
-    return errors
-}
+// const validate = values => {
+//     const errors = {}
+//     if (!values.email) {
+//         errors.email = 'Required'
+//     } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+//         errors.email = 'Invalid email address'
+//     }
+//     return errors
+// }
 
 export const App = () => {
     return (
--- src/components/PhoneNumberInput/PhoneNumberInput.js
+++ src/components/PhoneNumberInput/PhoneNumberInput.js
@@ -1,46 +1,66 @@
-import React, { useState } from 'react'
-import { TextField } from 'formik-mui'
+import React, { useState, useRef } from 'react'
+// import { TextField } from 'formik-mui'
 import { Field } from 'formik'
 import phoneNumberMask from '../../services/phoneNumberMask/phoneNumberMask'
+// import { MaskField } from 'react-mask-field'
+import TextField from '@mui/material/TextField'
+import 'react-phone-number-input/style.css'
+import PhoneInput from 'react-phone-number-input'
+import ua from 'react-phone-number-input/locale/ua.json'
+import Input from 'react-phone-number-input/input'
 
-console.log(
-    phoneNumberMask({
-        mask: '+x xxx xxx xx xx',
-        phone: '78905556781',
-        visible: false,
-    })
-)
+// import { TextField } from 'formik-mui'
+
+const MyTextField = React.forwardRef(function custom(props, ref) {
+    console.log(props)
+    return <TextField {...props} fullWidth inputRef={ref} label="Phone number" />
+})
+
+// const MyTextField = props => {
+//     console.log(props)
+//     return <TextField {...props} fullWidth />
+// }
 
 export const PhoneNumberInput = ({ text, name }) => {
     const [click, setClick] = useState(true)
-    const [entered, setEntered] = useState('')
+    const [entered, setEntered] = useState({ string: '' })
+    const inputText = useRef(null)
 
-    const handleChange = event => {
-        // setEntered(event.target.value)
-        setEntered(
-            phoneNumberMask({
-                mask: '+x xxx xxx xx xx',
-                phone: event.target.value,
-                visible: false,
-            })
-        )
-    }
+    const [value, setValue] = useState()
 
-    const handleClick = () => {
-        click && setEntered('+7')
-        setClick(false)
-    }
+    // const handleChange = event => {
+    //     // setEntered(event.target.value)
+    //     setEntered({
+    //         string: phoneNumberMask({
+    //             mask: '+x xxx xxx xx xx',
+    //             phone: event.target.value,
+    //             visible: false,
+    //         }),
+    //     })
+    // }
 
     return (
-        <Field
-            component={TextField}
-            label={text}
-            value={entered}
-            name={name}
-            variant="outlined"
-            fullWidth
-            onChange={handleChange}
-            onClick={handleClick}
-        />
+        <>
+            {/*<Field*/}
+            {/*    component={TextFieldWithMask}*/}
+            {/*    label={text}*/}
+            {/*    value={entered.string}*/}
+            {/*    name={name}*/}
+            {/*    variant="outlined"*/}
+            {/*    fullWidth*/}
+            {/*    onChange={handleChange}*/}
+            {/*    // onClick={handleClick}*/}
+            {/*/>*/}
+            {/*<TextFieldWithMask />*/}
+            {/*<PhoneInput placeholder="Enter phone number" value={value} onChange={setValue} />*/}
+            <Input
+                country="UA"
+                international
+                value={value}
+                withCountryCallingCode
+                onChange={setValue}
+                inputComponent={MyTextField}
+            />
+        </>
     )
 }
